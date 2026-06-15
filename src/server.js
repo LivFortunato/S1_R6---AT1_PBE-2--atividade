4ade0594baea938e3ed8d83cfc58d21a16d7dc4a
LivFortunato <livia.fortunatofl@gamil.com>
2026-06-15 16:00:59 -0300

    correçao 02

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes.js';
 import { initializeDatabase } from './configs/Database.js';
+import cors from 'cors';
+import path from 'path';
 
 
 const app = express();
-
+app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(cors());
 
 app.use(express.json());
 app.use('/', routes);
